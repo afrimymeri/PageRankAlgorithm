f644edb3b208dcdccefd62b4a98dde9162e4a37c
BrilantP <ponxhaj@pupil.ch>
2024-12-20 16:34:51 +0100

    Minor format changes

--- src/PageRank.java
+++ src/PageRank.java
@@ -5,28 +5,28 @@ public class PageRank {
     private static final double DEFAULT_DAMPING_FACTOR = 0.85;
     private static final int MAX_ITERATIONS = 100;
     private static final double CONVERGENCE_THRESHOLD = 1e-6;
-    public static Map<String,Double> computePageRank(Map<String, List<String>> graph, double dampingFactor) {
+
+    public static Map<String, Double> computePageRank(Map<String, List<String>> graph, double dampingFactor) {
         int numNodes = graph.size();
         Map<String, Double> ranks = new HashMap<>();
         Map<String, Double> newRanks = new HashMap<>();
 
         //Initialize each node with an equal PageRank score
         for (String node : graph.keySet()) {
-            ranks.put(node, 1.0/ numNodes);
+            ranks.put(node, 1.0 / numNodes);
         }
-        for (int i =0; i< MAX_ITERATIONS; i++) {
+        for (int i = 0; i < MAX_ITERATIONS; i++) {
             for (String node : graph.keySet()) {
-                double rankSum =0.0;
+                double rankSum = 0.0;
 
-                //Contributions of the incoming nodes
                 for (String neighbour : graph.keySet()) {
-                    if (graph.get(neighbour).contains(node)){
+                    if (graph.get(neighbour).contains(node)) {
                         rankSum += ranks.get(neighbour) / graph.get(neighbour).size();
                     }
                 }
 
                 //Update rank with damping factor
-                newRanks.put(node, (1-dampingFactor)/numNodes + dampingFactor * rankSum);
+                newRanks.put(node, (1 - dampingFactor) / numNodes + dampingFactor * rankSum);
             }
 
             //Check for convergence
@@ -41,11 +41,10 @@ public class PageRank {
                 break;
             }
 
-            //Update ranks for the next iterations
             ranks.putAll(newRanks);
         }
-        Map<String,Double> temporarily = new HashMap<>();
-        return sortByValue(ranks); // will change when the right method is added
+        Map<String, Double> temporarily = new HashMap<>();
+        return sortByValue(ranks);
     }
 
     /**
